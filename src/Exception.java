0b9a985d5855d4a36516315d95856f95d2a64009
Elise B. Dirheimer <elise.dirheimer@gmail.com>
2023-08-31 14:12:32 +0200

    FirstCommit

--- src/Exception.java
+++ src/Exception.java
@@ -1,2 +1,6 @@
-public class Exception {
+public class Exception extends RuntimeException{
+    java.lang.Exception Specialexception() {
+        System.out.println("Special exception !");
+        return null;
+    }
 }
