09c6661dca0ed0b0b9e7529aaecb33f97496b614
Elise B. Dirheimer <elise.dirheimer@gmail.com>
2023-08-31 14:47:56 +0200

    Setup_NouvelleFonctionnalite

--- src/NouvelleFonctionnalite.java
+++ src/NouvelleFonctionnalite.java
@@ -2,4 +2,18 @@ public class NouvelleFonctionnalite {
     boolean isOK;
     String name;
     int number;
+
+    public boolean isOK() {
+        return isOK;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
+
+
